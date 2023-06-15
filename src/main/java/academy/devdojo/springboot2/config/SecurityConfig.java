7cb4ea8f1307ffa4215c3ab58d852d1ffbcbb1e2
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2023-06-15 09:51:55 -0300

    Spring Security 2 CSRF Token

--- src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -17,7 +17,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz
+        http.csrf().disable()
+                .authorizeHttpRequests((authz) -> authz
                 .anyRequest()
                 .authenticated()
         ).httpBasic(withDefaults());
