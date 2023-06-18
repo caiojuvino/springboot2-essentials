b051160d2cea504fc2fd02c1e84f4e16a867aff9
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2023-06-17 21:55:15 -0300

    Spring Security 3 Segurança a nível de métodos com PreAuthorize

--- src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package academy.devdojo.springboot2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,9 +11,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
@@ -27,12 +32,24 @@ public class SecurityConfig {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
+        List<UserDetails> userDetailsList = new ArrayList<>();
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        UserDetails user = User
-                .withUsername("devdojo")
+
+        UserDetails user1 = User
+                .withUsername("admin")
                 .password(encoder.encode("academy"))
                 .roles("USER","ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(user);
+
+        UserDetails user2 = User
+                .withUsername("devdojo")
+                .password(encoder.encode("academy"))
+                .roles("USER")
+                .build();
+
+        userDetailsList.add(user1);
+        userDetailsList.add(user2);
+
+        return new InMemoryUserDetailsManager(userDetailsList);
     }
 }
